fe506079d5ce57d6b7242f4064c5f0ea20a9077f
roman89ua <roman89ua@gmail.com>
2019-11-07 20:26:46 +0200

    first commit. just saving first pisies of code

---
+++ script.js
@@ -0,0 +1,17 @@
+// random numbers for location the treasures
+let width = 900;
+let height = 600;
+let clicks = 0
+let randomNumber = function(size){
+    return Math.floor(Math.random() * xMax);
+}
+let target = {
+    x: rendomNumber(width),
+    y: rendomNumber(height)
+};
+
+document.getElementById('map').click = function(event){
+
+}
+
+
