441f5c1f13acb83595317d82553c4d446462221e
Roman Kostiuk <roman89ua@gmail.com>
2019-11-08 21:57:57 +0200

    end of the day

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 // random numbers for location the treasures
 let width = 900;
 let height = 600;
-let clicks = 0
-let randomNumber = function(size){
-    return Math.floor(Math.random() * xMax);
+let clicks = 0;
+let randomNumber = function(size) {
+    return Math.floor(Math.random() * size);
 }
 let target = {
-    x: rendomNumber(width),
-    y: rendomNumber(height)
+    x: randomNumber(width),
+    y: randomNumber(height)
 };
 
-document.getElementById('map').click = function(event){
+document.getElementById('map').click = function(event) {
 
 }
-
-
